38d104cdd97eff5cbd8456c6d2b8f80d98d06066
jamydevph <127085144+jamydevph@users.noreply.github.com>
2026-09-13 01:23:48 +0800

    feat: set up Firebase (mobile-only) + Firestore rules (Day 2)

--- tapikappflutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ tapikappflutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import bonsoir_darwin
+import cloud_firestore
+import firebase_auth
+import firebase_core
 import screen_retriever_macos
 import shared_preferences_foundation
 import tray_manager
@@ -13,6 +16,9 @@ import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SwiftBonsoirPlugin.register(with: registry.registrar(forPlugin: "SwiftBonsoirPlugin"))
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   TrayManagerPlugin.register(with: registry.registrar(forPlugin: "TrayManagerPlugin"))
--- tapikappflutter/windows/flutter/generated_plugins.cmake
+++ tapikappflutter/windows/flutter/generated_plugins.cmake
@@ -4,6 +4,9 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   bonsoir_windows
+  cloud_firestore
+  firebase_auth
+  firebase_core
   screen_retriever_windows
   tray_manager
   window_manager
