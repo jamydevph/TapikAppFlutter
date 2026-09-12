19ec0d8158a9187a04fa5d51bba670070352aebe
jamydevph <127085144+jamydevph@users.noreply.github.com>
2026-09-12 21:54:18 +0800

    feat: scaffold project foundation and role switch (Day 1)

--- tapikappflutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ tapikappflutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import bonsoir_darwin
+import screen_retriever_macos
+import shared_preferences_foundation
+import tray_manager
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SwiftBonsoirPlugin.register(with: registry.registrar(forPlugin: "SwiftBonsoirPlugin"))
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  TrayManagerPlugin.register(with: registry.registrar(forPlugin: "TrayManagerPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
